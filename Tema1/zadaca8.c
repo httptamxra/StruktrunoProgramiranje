40f201ab173c325adcf9ec972b565d89e07b315b
httptamxra <136267659+httptamxra@users.noreply.github.com>
2023-09-14 14:26:43 +0200

    Create zadaca8.c

---
+++ Tema1/zadaca8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+//Napishi programa kade shto preku tastatura kje se vnesat a,b,h.
+// Pronajdi ja nivanata plostina so formulata.
+int main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    scanf ("%d", &b);
+    scanf ("%d", &c);
+    int P = (a+b)/2 *2;
+    printf ("The area is: %d", P);
+    return 0;
+}

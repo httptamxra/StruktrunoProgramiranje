2dcc99edf99bccf3c608bdae22ec93981c2f733d
httptamxra <136267659+httptamxra@users.noreply.github.com>
2023-09-14 14:38:32 +0200

    Create zadaca9.c

---
+++ Tema1/zadaca9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+//Napishi programa kade shto preku tastatura kje se vnesi r.
+// Presmetaj ja vrednosta na C so formulata.
+int main()
+{
+    int r;
+    int pi = 3.14;
+    scanf("%d", &r);
+    int C = 2*pi*r;
+    printf("The value of C is %d", C);
+    return 0;
+}

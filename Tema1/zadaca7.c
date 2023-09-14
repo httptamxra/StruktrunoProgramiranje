c008eb58e2bd9c1eafe0b1f045a02f5eb5f8b023
httptamxra <136267659+httptamxra@users.noreply.github.com>
2023-09-14 14:10:22 +0200

    Create zadaca7.c

---
+++ Tema1/zadaca7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Napishi programa kade shto preku tastatura kje se vnesat b,h.
+// Pronajdi ja nivanata plostina so formulata.
+int main()
+{
+    int b,h;
+    scanf("%d", &b);
+    scanf("%d", &h);
+    int P = b*h;
+    printf("The area of A is: %d",P);
+    return 0;
+}
